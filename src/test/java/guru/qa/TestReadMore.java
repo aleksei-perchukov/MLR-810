2f215025eb2961b9c333054ae548fb26bdbb5488
aleksei.perchukov <aleksei.perchukov@gmail.com>
2022-09-27 18:12:59 +0300

    Final

--- src/test/java/guru/qa/TestReadMore.java
+++ src/test/java/guru/qa/TestReadMore.java
@@ -1,6 +1,7 @@
 package guru.qa;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -12,11 +13,11 @@ public class TestReadMore {
     public static String desktopResolution = "1920x1080";
     public static String mobileResolution = "390x844";
     public static String tabletResolution = "820x1180";
-    static String remote = System.getProperty("selenide.remote");
-    public static Boolean needToRebaseDesktopCSV = true;
-    public static Boolean needToRebaseMobileCSV = true;
-    public static Boolean needToRebaseTabletCSV = true;
+    public static Boolean needToRebaseDesktopCSV = Boolean.valueOf(System.getProperty("needToRebaseDesktopCSV"));
+    public static Boolean needToRebaseMobileCSV = Boolean.valueOf(System.getProperty("needToRebaseMobileCSV"));
+    public static Boolean needToRebaseTabletCSV = Boolean.valueOf(System.getProperty("needToRebaseMobileCSV"));
 
+    @Tag("DesktopTests")
     @CsvFileSource (files = writeCSVPathDesktop)
     @ParameterizedTest (name = "{0}{1} contains 'READ MORE' on DESKTOP (1280x1024)")
     void readMore_desktop (String storePrefix, String url_path) {
@@ -25,6 +26,7 @@ public class TestReadMore {
         System.out.println(storePrefix + url_path + ", DESKTOP OK");
     }
 
+    @Tag("MobileTests")
     @CsvFileSource (files = writeCSVPathMobile)
     @ParameterizedTest (name = "{0}{1} contains 'READ MORE' on MOBILE (390x844)")
     void readMore_mobile (String storePrefix, String url_path) {
@@ -33,6 +35,7 @@ public class TestReadMore {
         System.out.println(storePrefix + url_path + ", MOBILE OK");
     }
 
+    @Tag("TabletTests")
     @CsvFileSource (files = writeCSVPathTablet)
     @ParameterizedTest (name = "{0}{1} contains 'READ MORE' on TABLET (820x1180)")
     void readMore_tablet (String storePrefix, String url_path) {
